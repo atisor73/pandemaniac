fff695c9e0fb39d4c05aae4373c75fc8f3711082
atisor73 <rosita.fu99@gmail.com>
2022-02-14 17:29:26 -0800

    Adding viz_tournament

--- pandemaniac/visualizer.py
+++ pandemaniac/visualizer.py
@@ -147,6 +147,108 @@ def viz(G, seeds,
     return pn.Column(range_slider, i_slider, plotter)
 
 
+def viz_tournament(G, players,
+        palette=['#cb4f70', '#1b718c', '#779e1a', '#f49044',
+                '#46308d', '#2ea58e', '#e97d86', '#7e92bd',
+                '#c21019', '#f56327', '#fab01d', '#026f69',
+                '#750a2b', '#1b9e77', '#d95f02', '#7570b3',
+                '#e7298a', '#66a61e'],
+        uncolored_size=2.5,
+        colored_size=4.5,
+        layout=None,
+        ):
+    """
+    Arguments
+    -------------------------------------
+    G : networkx Graph object
+    players : dictionary of {player names: chosen seeds}
+    palette : list of strings (hex values or html names)
+    uncolored_size : float, size of uncolored glyphs
+    colored_size : float, size of colored glyphs
+    layout : string ("radial", "random", "kamada", "spectral"),
+             default "spring"
+
+    Returns :
+    -------------------------------------
+    Bokeh panel object displaying color cascade, with cycle+iteration, and zoom control.
+    """
+    # ***************** GRAPH LAYOUT *****************
+    print("graph layout... ")
+    if layout == "radial":     layout = nx.circular_layout(G)
+    elif layout == "random":   layout = nx.random_layout(G)
+    elif layout == "kamada":   layout = nx.kamada_kawai_layout(G)
+    elif layout == "spectral": layout = nx.spectral_layout(G)
+    else:                      layout = nx.spring_layout(G)
+
+    df = dataframer(layout)
+    
+    # ********* SIMULATING 50 CYCLES LAYOUT *********
+    print("simulating... ")
+    names = list(players.keys())
+    all_seeds = list(players.values())
+    
+    N_CYCLES = 50
+    results, histories = [], []
+    for cycle in range(N_CYCLES):
+        seeds = [team[cycle] for team in all_seeds]
+        result, history = simulate(G, seeds)
+        results.append(result)
+        histories.append(history)
+    
+    color_map = {k:v for k, v in zip(result.keys(), palette)}
+    color_map[None] = "grey"
+    print()
+    size_map = {None : uncolored_size}
+    for k in result.keys():
+        size_map[k] = colored_size
+
+    cycle_selector = pn.widgets.Select(name="cycle", width=90,
+        options=list(np.arange(N_CYCLES)), value=0)
+    i_slider = pn.widgets.IntSlider(start=1, end=len(histories[0]), value=1,
+                                    name="iteration", width=290)
+    range_slider = pn.widgets.FloatSlider(name='zoom', width=400,
+                                      start=0.10, end=2.0, value=1.1, step=0.05)
+    
+    @pn.depends(cycle_selector.param.value, watch=True)
+    def update_i_end(cycle):
+        i_slider.end = len(histories[cycle])
+        
+    @pn.depends(cycle_selector.param.value, i_slider.param.value, range_slider.param.value)
+    def plotter(cycle, i, z):
+        result, history = results[cycle], histories[cycle]
+        z = 2.05 - z
+        df_ = df
+        p = bokeh.plotting.figure(title=f"iteration {i}",
+                                  width=600, height=450,
+                                  x_range=[-z,z],y_range=[-z, z],  )
+        
+        max_i = len(history)-1
+        _index = max_i if i > max_i else i-1 
+        
+        # iterating, update color and size of node
+        labels = list(history[_index].values())
+        df_["color"] = [color_map[label] for label in labels]
+        df_["size"] = [size_map[label] for label in labels]
+        df_ = df_.sort_values(by=["size"])
+
+        # creating legend
+        if names==None:
+            items = [(f"team {k}", [p.circle(0,0, color=f"{v}")])
+                                        for k, v in color_map.items()]
+        else:
+            items = [(f"{list(names)[i]}", [p.circle(0,0, color=f"{kv[1]}")])
+                                        for i, kv in enumerate(color_map.items())
+                                        if i < len(seeds)
+                                        ]
+        legend = bokeh.models.Legend(items=items, location="center")
+        p.add_layout(legend, 'right')
+
+        # only plotting nodes
+        p.circle(source=df_, x='x',y='y', color='color', size='size',
+                 line_color="white", line_width=0.1)
+        p.toolbar_location="below"
+        return style(p)
+    return pn.Column(range_slider, pn.Row(cycle_selector, i_slider), plotter)
 
 def ecdf_rank(opponents, my_rank,
               palette=['#cb4f70', '#1b718c', '#779e1a', '#f49044',
